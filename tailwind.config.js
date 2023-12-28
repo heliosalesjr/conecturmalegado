f06b621215b71536f23f0573d58b0eea5388f4ba
Helio Sales Jr <heliosalesjr@gmail.com>
2023-12-28 20:46:18 +0800

    color pallete updated (tailwind config)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      
+      colors: {
+        'light-shade': '#f0f3e5',
+        'light-accent': '#b8b9ac',
+        'main-primary': '#ed44f1',
+        'dark-accent': '#7d6c9d',
+        'dark-shades': '#36597a',
+      },
     },
   },
   plugins: [],
