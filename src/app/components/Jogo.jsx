76373fc1f1f4150da8071b31f72a0444d999ab51
allankovac <allankfrasco@gmail.com>
2024-02-19 12:40:29 -0300

    implementando o jogo

--- src/app/components/Jogo.jsx
+++ src/app/components/Jogo.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 function Jogo() {
   return (
     <div id="jogo" className='hidden sm:flex items-center justify-center h-screen bg-blue-100'>
-        <h1 className='text-5xl align-middle font-semibold'>
-        O Jogo vem aqui!
-        </h1>
+      <iframe src="./jogo/iframe.html" width="1000px" height="600px"></iframe>
     </div>
   )
 }
